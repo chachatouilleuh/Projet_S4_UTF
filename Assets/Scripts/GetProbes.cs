f0fe4f542be39a9ea4f20ae3962d05c5e7e842a5
Khenan <59953532+maxime-delhomme@users.noreply.github.com>
2022-03-18 14:17:11 +0100

    getProbes via scriptableObject

--- Assets/Scripts/GetProbes.cs
+++ Assets/Scripts/GetProbes.cs
@@ -8,11 +8,20 @@ public class GetProbes : MonoBehaviour
     [SerializeField, Tooltip("le layer des sondes")]
     private LayerMask m_layerProbes;
 
+    [SerializeField, Tooltip("inventaire")]
+    private List<ProbesType> m_inventaire = new List<ProbesType>();
     private void OnTriggerEnter(Collider other)
     {
         if ((m_layerProbes.value & (1 << other.gameObject.layer)) > 0)
         {
             Probes myProbes = other.GetComponent<Probes>();
+            if (myProbes != null && myProbes.GetProbes(out ProbesType probes))
+            {
+                if (!m_inventaire.Contains(probes))
+                {
+                    m_inventaire.Add(probes);
+                }
+            }
         }
     }
 }
