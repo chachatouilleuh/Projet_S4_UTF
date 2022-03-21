6a5f1601a57e7ada5384f5349330e0f170f89b59
Khenan <59953532+maxime-delhomme@users.noreply.github.com>
2022-03-21 09:23:46 +0100

    fetching to dev mamene

---
+++ Assets/Scripts/Pick_Object.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class Pick_Object : MonoBehaviour
+{
+    [SerializeField, Tooltip("le layer de l'objet")] private LayerMask m_pickable_Object;
+    [SerializeField, Tooltip("la caméra du perso")] Camera fpsCam;
+    [SerializeField, Tooltip("la range pour pick l'objet")] private float m_distance;
+    [SerializeField, Tooltip("recup le transform de la 'main'")] private Transform m_hand;
+    
+    private Rigidbody m_rigidbody;
+    private Collider m_collider;
+    
+    private void Update()
+    {
+        Ray pickupRay = new Ray(fpsCam.transform.position, fpsCam.transform.forward * m_distance);
+
+        RaycastHit hit;
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(pickupRay, out hit, m_distance, m_pickable_Object))
+            {
+                if (!m_rigidbody)
+                {
+                    m_rigidbody = hit.rigidbody;
+                    m_collider = hit.collider;
+        
+                    m_rigidbody.isKinematic = true;
+                    m_collider.enabled = false;
+                }
+                return;
+            }
+        
+            if (m_rigidbody)
+            {
+                m_rigidbody.isKinematic = false;
+                m_collider.enabled = true;
+            
+                m_rigidbody = null;
+                m_collider = null;
+            }
+        }
+        
+        if (m_rigidbody)
+        {
+            m_rigidbody.position = m_hand.position;
+            m_rigidbody.rotation = m_hand.rotation;
+        }
+    }
+}
--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using Packages.QuickOutline.Scripts;
-using UnityEditorInternal;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class Raycast : MonoBehaviour
 {
-    [SerializeField, Tooltip("le layer de l'objet")] private LayerMask m_pickable_Object;
+    [SerializeField, Tooltip("le layer de l'objet")] private LayerMask m_oultine_Object;
     [SerializeField, Tooltip("la caméra du perso")] Camera fpsCam;
     [SerializeField, Tooltip("la range pour pick l'objet")] private float m_distance;
     [SerializeField, Tooltip("recup le transform de la 'main'")] private Transform m_hand;
@@ -18,16 +13,14 @@ public class Raycast : MonoBehaviour
     private Collider m_collider;
     void Update()
     {
-        // Vector3 m_direction = new Vector3(1f, 0f, 1f);
-
         Debug.DrawRay(fpsCam.transform.position, fpsCam.transform.forward * m_distance, Color.green);
+        
         Ray pickupRay = new Ray(fpsCam.transform.position, fpsCam.transform.forward * m_distance);
-
         RaycastHit hit;
 
-        if (Physics.Raycast(pickupRay, out hit, m_distance))
+        if (Physics.Raycast(pickupRay, out hit, m_distance, m_oultine_Object))
         {
-            if (hit.transform.gameObject.CompareTag("Pickable_Object") || hit.transform.gameObject.CompareTag("Probes_Object") || hit.transform.gameObject.CompareTag("Lock_Object"))
+            if (hit.transform.gameObject)
             {
                 m_curTarget = hit.transform.gameObject;
                 m_curTarget.GetComponent<Outline>().enabled = true;
@@ -46,37 +39,6 @@ public class Raycast : MonoBehaviour
                 return;
             }
             m_curTarget.GetComponent<Outline>().enabled = false;
-        }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (Physics.Raycast(pickupRay, out hit, m_distance, m_pickable_Object))
-            {
-                if (!m_rigidbody)
-                {
-                    m_rigidbody = hit.rigidbody;
-                    m_collider = hit.collider;
-        
-                    m_rigidbody.isKinematic = true;
-                    m_collider.enabled = false;
-                }
-                return;
-            }
-        
-            if (m_rigidbody)
-            {
-                m_rigidbody.isKinematic = false;
-                m_collider.enabled = true;
-            
-                m_rigidbody = null;
-                m_collider = null;
-            }
-        }
-        
-        if (m_rigidbody)
-        {
-            m_rigidbody.position = m_hand.position;
-            m_rigidbody.rotation = m_hand.rotation;
         }
     }
 }
