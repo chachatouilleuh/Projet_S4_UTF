7ad8b56101e1ec01c21ead379ff3f4984186bc1e
maxime <59953532+maxime-delhomme@users.noreply.github.com>
2022-03-28 09:53:40 +0200

    Update Pick_Object.cs

--- Assets/Scripts/Pick_Object.cs
+++ Assets/Scripts/Pick_Object.cs
@@ -17,13 +17,9 @@ public class Pick_Object : MonoBehaviour
     [SerializeField, Tooltip("recup le transform de la 'main'")] 
     private Transform m_hand;
 
-    [SerializeField, Tooltip("bool pour check si on porte un objet")] 
-    private bool m_gotBox = false;
-    
     private Rigidbody m_rigidbody;
     private Collider m_collider;
-    private Transform m_transform;
-    
+
     private void Update()
     {
         Ray pickupRay = new Ray(fpsCam.transform.position, fpsCam.transform.forward * m_distance);
@@ -41,45 +37,17 @@ public class Pick_Object : MonoBehaviour
         
                     m_rigidbody.isKinematic = true;
                     m_collider.enabled = false;
-
-                    m_gotBox = true;
-                    Debug.Log("j'ai une boite");
                 }
                 return;
             }
         
-            // if (m_rigidbody)
-            // {
-            //     m_rigidbody.isKinematic = false;
-            //     m_collider.enabled = true;
-            //
-            //     m_rigidbody = null;
-            //     m_collider = null;
-            //
-            //     m_gotBox = false;
-            //     Debug.Log("j'ai plus de boite");
-            // }
-
-            if (Physics.Raycast(pickupRay, out hit, m_distance, m_pressPlate))
+            if (m_rigidbody)
             {
-                if (m_gotBox == true)
-                {
-                    Debug.Log("j'ai pose la boite");
-                    if (m_rigidbody)
-                    {
-                        m_transform = hit.transform;
-
-                        // m_rigidbody.isKinematic = false;
-                        // m_collider.enabled = true;
-                        //
-                        // m_rigidbody = null;
-                        // m_collider = null;
-                    
-                        m_gotBox = false;
-                        Debug.Log("j'ai plus de boite");
-                    }
-                }
-                Debug.Log("j'ai pas de boite");
+                m_rigidbody.isKinematic = false;
+                m_collider.enabled = true;
+            
+                m_rigidbody = null;
+                m_collider = null;
             }
         }
 
