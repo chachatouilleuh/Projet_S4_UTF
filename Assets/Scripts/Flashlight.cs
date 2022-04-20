f2412810763323d3c918e97f2eb0843dd2e36103
maxime-delhomme <59953532+maxime-delhomme@users.noreply.github.com>
2022-04-20 11:02:03 +0200

    debut ld

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -7,12 +7,7 @@ using UnityEngine;
 public class Flashlight : MonoBehaviour
 {
     [SerializeField, Tooltip("flash active ?")] private bool m_isOn;
-    [SerializeField, Tooltip("layer gravityFields")] private LayerMask m_fieldLayer;
-    [SerializeField, Tooltip("la caméra du perso")] Camera fpsCam;
-    [SerializeField, Tooltip("la range pour pick l'objet")] private float m_distance;
-    [SerializeField] private GameObject m_curField;
-    [SerializeField] private MeshCollider m_curFieldComp;
-    
+
 
     private void Start()
     {
@@ -22,10 +17,7 @@ public class Flashlight : MonoBehaviour
 
     private void Update()
     {
-        Debug.DrawRay(fpsCam.transform.position, fpsCam.transform.forward * m_distance, Color.green);
-        Ray pickupRay = new Ray(fpsCam.transform.position, fpsCam.transform.forward * m_distance);
-        RaycastHit hit;
-        
+
         if (!Input.GetKeyDown(KeyCode.E)) return;
         switch (m_isOn)
         {
@@ -37,27 +29,6 @@ public class Flashlight : MonoBehaviour
                 m_isOn = false;
                 GetComponent<Light>().enabled = false;
                 break;
-        }
-
-        if (Physics.Raycast(pickupRay, out hit, m_distance, m_fieldLayer))
-        {
-            if (hit.transform.gameObject)
-            {
-                m_curField = hit.transform.gameObject;
-                m_curFieldComp = m_curField.GetComponent<MeshCollider>();
-                Debug.Log(m_curFieldComp);
-                if (m_isOn)
-                {
-                    Debug.Log("1");
-                    m_curFieldComp.enabled = false;
-                }
-                
-                if(!m_isOn)
-                {
-                    Debug.Log("2");
-                    m_curFieldComp.enabled = true;
-                }
-            }
         }
     }
 }
