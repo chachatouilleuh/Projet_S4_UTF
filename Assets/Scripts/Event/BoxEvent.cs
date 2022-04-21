25e2a35c0ed6762f7995d619e87c3f442d6616a1
maxime-delhomme <59953532+maxime-delhomme@users.noreply.github.com>
2022-04-21 12:14:27 +0200

    fin ld puzzle 1

--- Assets/Scripts/Event/BoxEvent.cs
+++ Assets/Scripts/Event/BoxEvent.cs
@@ -11,6 +11,9 @@ public class BoxEvent : MonoBehaviour
     [SerializeField, Tooltip("recup le gameobject")]
     private Rigidbody m_boxTriggered;
 
+    [SerializeField, Tooltip("layer pont")]
+    private LayerMask m_bridgeMask;
+
     private Coroutine m_boxEventCor;
 
     [SerializeField, Tooltip("temps avant de detruire objet")]
@@ -50,5 +53,13 @@ public class BoxEvent : MonoBehaviour
         yield return new WaitForSeconds(m_SecondsWait);
         
         Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision p_collision)
+    {
+        if ((m_bridgeMask.value & (1 << p_collision.gameObject.layer)) > 0)
+        {
+            p_collision.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        }
     }
 }
