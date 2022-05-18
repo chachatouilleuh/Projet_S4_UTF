7c4ea567c6b01fb0c4a44c346135eb79e4fa35ae
chachatouilleuh <charlydespeyroux@gmail.com>
2022-05-19 01:11:30 +0200

    DEFILEMENT OK !!!!!

--- Assets/Scripts/UI/FadeInOut.cs
+++ Assets/Scripts/UI/FadeInOut.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class FadeInOut : MonoBehaviour
 {
-    [SerializeField, Tooltip("le canvas des sous-titres")] private GameObject m_subtitleCanvas ;
-    [SerializeField, Tooltip("le canvas noir de transition")] private MaskableGraphic m_blackCanvas ;
-
     [SerializeField, Tooltip("layer du player")] private LayerMask m_playerLayer;
-    
     [SerializeField, Tooltip("canvas est un dialogue")] private bool m_isDialogue;
-    [SerializeField, Tooltip("le canvas est déjà affiché")] private bool m_alreadyPlayed;
+    
+    [SerializeField, Tooltip("le canvas noir de transition")] private MaskableGraphic m_blackCanvas ;
+    [SerializeField, Tooltip("le temps de fade")]private float m_fadeTime;
+    
     
     [SerializeField, Tooltip("fade est déjà joué")]private bool m_alreadyFaded;
-    [SerializeField, Tooltip("le temps de fade")]private float m_fadeTime;
+
+    
+    [SerializeField, Tooltip("le canvas des sous-titres")] private GameObject m_subtitleCanvas ;
+    [SerializeField, Tooltip("le texte des sous-titres")] TMP_Text m_textToDisplay;
+    
     [SerializeField, Tooltip("le temps de fade")]private float m_subtitleTime;
+    [SerializeField, Tooltip("pause à chaque point")]private float dotPause;
+    [SerializeField, Tooltip("pause à chaque virgule")]private float commaPause;
+    [SerializeField, Tooltip("pause à chaque espace")]private float spacePause;
+    [SerializeField, Tooltip("pause entre caractères")]private float normalPause;
+    
+    [SerializeField, Tooltip("le canvas est déjà affiché")] private bool m_alreadyPlayed;
+   
     
     
     private void Start()
@@ -40,12 +51,13 @@ public class FadeInOut : MonoBehaviour
                 if (Underlining.m_sub == 0)
                 {
                     m_subtitleCanvas.SetActive(true);
+                    StartCoroutine(TypeSentence(m_textToDisplay.text));
                     StartCoroutine(HideSubtitles());
                 }  
             }
             else
             {
-                fadeIn();
+                FadeIn();
             }
         }
         m_alreadyPlayed = true;
@@ -63,9 +75,38 @@ public class FadeInOut : MonoBehaviour
         m_alreadyFaded = true;
     }
     
-    void fadeIn()
+    void FadeIn()
     {
         m_fadeTime += Time.deltaTime;
         m_blackCanvas.CrossFadeAlpha(1, 0.5f, false);
+    }
+
+    IEnumerator TypeSentence (string sentence){
+        m_textToDisplay.text ="";
+        Debug.Log("je suis entré dans la fonction de typeSentence");
+        foreach (char letter in sentence.ToCharArray()){
+            m_textToDisplay.text += letter;
+            yield return StartCoroutine(PauseBetweenChars(letter));
+        }
+    }
+
+    private IEnumerator PauseBetweenChars(char letter)
+    {
+        Debug.Log("je suis entré dans la fonction de pauses");
+        switch (letter)
+        {
+            case '.':
+                yield return new WaitForSeconds(dotPause);
+                break;
+            case ',':
+                yield return new WaitForSeconds(commaPause);
+                break;
+            case ' ':
+                yield return new WaitForSeconds(spacePause);
+                break;
+            default:
+                yield return new WaitForSeconds(normalPause);
+                break;
+        }
     }
 }
