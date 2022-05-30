c79b159d7954ddb43242fdde9f3aab9247e5fc3b
yaa <59953532+maxime-delhomme@users.noreply.github.com>
2022-05-30 16:05:40 +0200

    screen shake :alien:

--- Assets/Scripts/Event/CameraShake.cs
+++ Assets/Scripts/Event/CameraShake.cs
@@ -11,6 +11,12 @@ public class CameraShake : MonoBehaviour
 
     private Coroutine m_shakeCam;
 
+    [SerializeField, Tooltip("curve shake")]
+    private AnimationCurve m_curve;
+
+    [SerializeField, Tooltip("duration shake")]
+    private float m_duration;
+
     private void OnEnable()
     {
         m_triggeredEvent.onTrigger += HandleTriggerEvent;
@@ -34,22 +40,21 @@ public class CameraShake : MonoBehaviour
 
     public IEnumerator Shake(float duration, float magnitude)
     {
-        Vector3 originalPos = transform.localPosition;
+        Vector3 startPosition = transform.localPosition;
 
         float elapsed = 0.0f;
 
         while (elapsed < duration)
         {
-            float x = Random.Range(-1f, 1f) * magnitude;
-            float y = Random.Range(-1f, 1f) * magnitude;
-
-            transform.localPosition = new Vector3(x, y, originalPos.z);
-
             elapsed += Time.deltaTime;
+
+            float Strength = m_curve.Evaluate(elapsed / duration);
+
+            transform.localPosition = startPosition + Random.insideUnitSphere * Strength;
             
             yield return null;
         }
 
-        transform.localPosition = originalPos;
+        transform.localPosition = startPosition;
     }
 }
