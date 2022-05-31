c5498a26cf532fd328ba89e55498b7ddc173bbda
chachatouilleuh <charlydespeyroux@gmail.com>
2022-05-31 19:10:29 +0200

    debut loadscreen

---
+++ Assets/Scripts/UI/Loadscreen.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loadscreen : MonoBehaviour
+{
+    public static List<AsyncOperation> l_scenesLoading = new List<AsyncOperation>();
+
+    //private void Awake()
+    //{
+    //    UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName: "Menu", LoadSceneMode.Additive); 
+    //}
+}
