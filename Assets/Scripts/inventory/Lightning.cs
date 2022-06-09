0b9458affaee6046d3cc15e97517714e276d9f13
user <73894722+RookieHoi@users.noreply.github.com>
2022-06-09 17:53:41 +0200

    Lamps On

--- Assets/Scripts/inventory/Lightning.cs
+++ Assets/Scripts/inventory/Lightning.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,7 +10,20 @@ public class Lightning : MonoBehaviour, ILightning
     
     [SerializeField, Tooltip("clé pour activer")]
     private List<KeyType> m_key = new List<KeyType>();
-    
+
+    [SerializeField] private Material m_lampOff;
+    [SerializeField] private Material m_lampOn;
+    [SerializeField] private List<GameObject> l_spotLights;
+
+    private void Start()
+    {
+        gameObject.GetComponent<MeshRenderer>().material = m_lampOff;
+        foreach (var spotties in l_spotLights)
+        {
+            spotties.SetActive(false);
+        }
+    }
+
     private void OnEnable()
     {
         m_triggeredEvent.onTriggered += HandleTriggerEvent;
@@ -39,7 +53,11 @@ public class Lightning : MonoBehaviour, ILightning
                 return;
             }
         }
-        
         Debug.Log("Active la lumière + émissive");
+        gameObject.GetComponent<MeshRenderer>().material = m_lampOn;
+        foreach (var spotties in l_spotLights)
+        {
+            spotties.SetActive(true);
+        }
     }
 }
